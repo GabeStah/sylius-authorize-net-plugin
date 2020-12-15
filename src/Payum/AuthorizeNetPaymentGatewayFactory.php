9a28536891a673ce4424760f0cfb8d608b6bd906
Gabe Wyatt <gabe@solarixdigital.com>
2020-12-15 11:36:35 -0800

    ci: update composer.lock for deprecated sylius-authorize-net reference

--- src/Payum/AuthorizeNetPaymentGatewayFactory.php
+++ src/Payum/AuthorizeNetPaymentGatewayFactory.php
@@ -18,22 +18,9 @@ final class AuthorizeNetPaymentGatewayFactory extends GatewayFactory
       'payum.factory_title' => 'Authorize.net Payment',
       'payum.template.obtain_credit_card' =>
         '@SolarixSyliusAuthorizeNetPlugin/obtainCreditCard.html.twig',
-      // API template
-      //      'payum.template.api_request' =>
-      //        '@SolarixSyliusAuthorizeNetPlugin/api_request.html.twig',
-      //      'payum.action.authorize' => new AuthorizeAction(),
-      //      'payum.action.refund' => new RefundAction(),
-      //      'payum.action.cancel' => new CancelAction(),
-      //      'payum.action.notify' => new NotifyAction(),
       'payum.action.capture' => new CaptureAction(new Client()),
       'payum.action.convert_payment' => new ConvertPaymentAction(),
       'payum.action.status' => new StatusAction(),
-      // Payment form
-      //      'payum.action.api.payment_form' => function (ArrayObject $config) {
-      //        return new PaymentFormAction($config['payum.template.api_request']);
-      //      },
-      // Obtain credit card action
-      //      'payum.action.obtain_credit_card' => new ObtainCreditCardAction(),
     ]);
 
     if (false == $config['payum.api']) {
@@ -44,10 +31,10 @@ final class AuthorizeNetPaymentGatewayFactory extends GatewayFactory
         'payum.template.obtain_credit_card' =>
           '@SolarixSyliusAuthorizeNetPlugin/obtainCreditCard.html.twig',
       ];
-      $config->defaults($config['payum . default_options']);
-      $config['payum . required_options'] = ['api_id', 'transaction_key'];
+      $config->defaults($config['payum.default_options']);
+      $config['payum.required_options'] = ['api_id', 'transaction_key'];
 
-      $config['payum . api'] = function (ArrayObject $config) {
+      $config['payum.api'] = function (ArrayObject $config) {
         $config->validateNotEmpty($config['payum . required_options']);
 
         $api = new AuthorizeNetApi(
