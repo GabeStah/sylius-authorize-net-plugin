53cc3c73a48092a3ed36a265989315f2147671f5
Gabe Wyatt <gabe@solarixdigital.com>
2020-12-15 09:47:40 -0800

    feat: add full Authorize.net API support
    Payment data obtained at the end of Sylius checkout.  Authorization and
    capture handled with Authorize.net API via `AuthorizeNetApi` and related
     Payum Actions.  After auth and capture `AuthorizeNetResponse` is
     generated and determines status based on returned status code/errors.
    
    Refs https://gitlab.solarixdigital.com/solarix/voyager/raritan/ecommerce/-/issues/1
    
    feat: override obtain credit card form to retain store layout and theme
    
    chore: adjust default config to match Authorize.net naming scheme
    
    fix: authorization Action no longer default

--- src/Payum/AuthorizeNetPaymentGatewayFactory.php
+++ src/Payum/AuthorizeNetPaymentGatewayFactory.php
@@ -5,12 +5,8 @@ namespace Solarix\SyliusAuthorizeNetPlugin\Payum;
 use GuzzleHttp\Client;
 use Payum\Core\Bridge\Spl\ArrayObject;
 use Payum\Core\GatewayFactory;
-use Solarix\SyliusAuthorizeNetPlugin\Payum\Action\AuthorizeAction;
-use Solarix\SyliusAuthorizeNetPlugin\Payum\Action\CancelAction;
 use Solarix\SyliusAuthorizeNetPlugin\Payum\Action\CaptureAction;
 use Solarix\SyliusAuthorizeNetPlugin\Payum\Action\ConvertPaymentAction;
-use Solarix\SyliusAuthorizeNetPlugin\Payum\Action\NotifyAction;
-use Solarix\SyliusAuthorizeNetPlugin\Payum\Action\RefundAction;
 use Solarix\SyliusAuthorizeNetPlugin\Payum\Action\StatusAction;
 
 final class AuthorizeNetPaymentGatewayFactory extends GatewayFactory
@@ -20,17 +16,48 @@ final class AuthorizeNetPaymentGatewayFactory extends GatewayFactory
     $config->defaults([
       'payum.factory_name' => 'authorize_net_payment',
       'payum.factory_title' => 'Authorize.net Payment',
+      'payum.template.obtain_credit_card' =>
+        '@SolarixSyliusAuthorizeNetPlugin/obtainCreditCard.html.twig',
+      // API template
+      //      'payum.template.api_request' =>
+      //        '@SolarixSyliusAuthorizeNetPlugin/api_request.html.twig',
+      //      'payum.action.authorize' => new AuthorizeAction(),
+      //      'payum.action.refund' => new RefundAction(),
+      //      'payum.action.cancel' => new CancelAction(),
+      //      'payum.action.notify' => new NotifyAction(),
       'payum.action.capture' => new CaptureAction(new Client()),
-      'payum.action.authorize' => new AuthorizeAction(),
-      'payum.action.refund' => new RefundAction(),
-      'payum.action.cancel' => new CancelAction(),
-      'payum.action.notify' => new NotifyAction(),
-      'payum.action.status' => new StatusAction(),
       'payum.action.convert_payment' => new ConvertPaymentAction(),
+      'payum.action.status' => new StatusAction(),
+      // Payment form
+      //      'payum.action.api.payment_form' => function (ArrayObject $config) {
+      //        return new PaymentFormAction($config['payum.template.api_request']);
+      //      },
+      // Obtain credit card action
+      //      'payum.action.obtain_credit_card' => new ObtainCreditCardAction(),
     ]);
 
-    $config['payum.api'] = function () use ($config) {
-      return new AuthorizeNetApi($config['login_id']);
-    };
+    if (false == $config['payum.api']) {
+      $config['payum.default_options'] = [
+        'api_id' => $_ENV['AUTHORIZE_NET_API_ID'],
+        'transaction_key' => $_ENV['AUTHORIZE_NET_TRANSACTION_KEY'],
+        'sandbox' => true,
+        'payum.template.obtain_credit_card' =>
+          '@SolarixSyliusAuthorizeNetPlugin/obtainCreditCard.html.twig',
+      ];
+      $config->defaults($config['payum . default_options']);
+      $config['payum . required_options'] = ['api_id', 'transaction_key'];
+
+      $config['payum . api'] = function (ArrayObject $config) {
+        $config->validateNotEmpty($config['payum . required_options']);
+
+        $api = new AuthorizeNetApi(
+          $config['api_id'],
+          $config['transaction_key']
+        );
+        $api->setSandbox($config['sandbox']);
+
+        return $api;
+      };
+    }
   }
 }
