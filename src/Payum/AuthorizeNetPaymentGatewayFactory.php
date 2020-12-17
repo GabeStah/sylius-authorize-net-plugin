4735a8e6e7b2f35c14c71043fa4e833fae2c84c2
Gabe Wyatt <gabe@solarixdigital.com>
2020-12-17 11:33:43 -0800

    feat: add CMS plugin support
    feat: add legal and support pages
    
    Anti-Spam Policy, DMCA, Privacy Policy, Refund Policy (PLACEHOLDER), Terms of
    Service, About Us, FAQ, Limited Warranty, Manuals, and Need a Part

--- src/Payum/AuthorizeNetPaymentGatewayFactory.php
+++ src/Payum/AuthorizeNetPaymentGatewayFactory.php
@@ -11,23 +11,22 @@ use Solarix\SyliusAuthorizeNetPlugin\Payum\Action\StatusAction;
 
 final class AuthorizeNetPaymentGatewayFactory extends GatewayFactory
 {
-  protected function populateConfig(ArrayObject $config): void
-  {
+  protected function populateConfig(ArrayObject $config): void {
     $config->defaults([
-      'payum.factory_name' => 'authorize_net_payment',
-      'payum.factory_title' => 'Authorize.net Payment',
+      'payum.factory_name'                => 'authorize_net_payment',
+      'payum.factory_title'               => 'Authorize.net Payment',
       'payum.template.obtain_credit_card' =>
         '@SolarixSyliusAuthorizeNetPlugin/obtainCreditCard.html.twig',
-      'payum.action.capture' => new CaptureAction(new Client()),
-      'payum.action.convert_payment' => new ConvertPaymentAction(),
-      'payum.action.status' => new StatusAction(),
+      'payum.action.capture'              => new CaptureAction(new Client()),
+      'payum.action.convert_payment'      => new ConvertPaymentAction(),
+      'payum.action.status'               => new StatusAction(),
     ]);
 
     if (false == $config['payum.api']) {
       $config['payum.default_options'] = [
-        'api_id' => $_ENV['AUTHORIZE_NET_API_ID'],
-        'transaction_key' => $_ENV['AUTHORIZE_NET_TRANSACTION_KEY'],
-        'sandbox' => true,
+        'api_id'                            => $_ENV['AUTHORIZE_NET_API_ID'],
+        'transaction_key'                   => $_ENV['AUTHORIZE_NET_TRANSACTION_KEY'],
+        'sandbox'                           => true,
         'payum.template.obtain_credit_card' =>
           '@SolarixSyliusAuthorizeNetPlugin/obtainCreditCard.html.twig',
       ];
@@ -35,7 +34,7 @@ final class AuthorizeNetPaymentGatewayFactory extends GatewayFactory
       $config['payum.required_options'] = ['api_id', 'transaction_key'];
 
       $config['payum.api'] = function (ArrayObject $config) {
-        $config->validateNotEmpty($config['payum . required_options']);
+        $config->validateNotEmpty($config['payum.required_options']);
 
         $api = new AuthorizeNetApi(
           $config['api_id'],
